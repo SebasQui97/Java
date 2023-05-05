e2f36f6a23dc5ac4e38e39d6fc35cf42a100c8b8
QuinSDev <quindev97@gmail.com>
2023-05-05 14:46:54 -0500

    Se agrega el ejercicio3_Frase

---
+++ Ejercicio3_Frase/src/ejercicio3_frase/Ejercicio3_Frase.java
@@ -0,0 +1,18 @@
+package ejercicio3_frase;
+
+import java.util.Scanner;
+
+public class Ejercicio3_Frase {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String phrase;
+        
+        System.out.print("Ingresa por favor una frase: ");
+        phrase = read.nextLine();
+        
+        System.out.println("Frase en mayúscula: " + phrase.toUpperCase());
+        System.out.println("Frase en minúscula: " + phrase.toLowerCase());
+    }
+    
+}
