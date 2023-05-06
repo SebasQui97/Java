bce28d1d2b610bc4f98d969c442585dc1299069d
QuinSDev <quindev97@gmail.com>
2023-05-06 08:32:15 -0500

    Se modifica el mensaje en el main y se agrega el archivo .gitignore

--- Ejercicio3_Frase/src/ejercicio3_frase/Ejercicio3_Frase.java
+++ Ejercicio3_Frase/src/ejercicio3_frase/Ejercicio3_Frase.java
@@ -8,7 +8,7 @@ public class Ejercicio3_Frase {
         Scanner read = new Scanner(System.in);
         String phrase;
         
-        System.out.print("Por favor introduce una frase: ");
+        System.out.print("Introduce una frase: ");
         phrase = read.nextLine();
         
         System.out.println("Frase en mayúscula: " + phrase.toUpperCase());
